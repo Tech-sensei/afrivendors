df19541d782fbc704f0d905bbccb4856964914b6
Tobiloba Ibraheem <ibraheemtobiloba15@gmail.com>
2025-12-07 14:26:42 +0100

    work in progress...

--- src/app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function NotFound() {
-  redirect("/");
-
-  // This return statement is a fallback and won't be reached
-  return null;
-}
